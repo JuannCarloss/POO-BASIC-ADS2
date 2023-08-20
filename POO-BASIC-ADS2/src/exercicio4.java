a58105d6087180f684b6d8bf818726f222d22786
JuannCarloss <juancarlosbatistazeferino@gmail.com>
2023-08-20 20:54:03 -0300

    exerc-4

--- POO-BASIC-ADS2/src/exercicio4.java
+++ POO-BASIC-ADS2/src/exercicio4.java
@@ -9,5 +9,37 @@ public class exercicio4 {
         System.out.println(sobrenome);
 
         JOptionPane.showMessageDialog(null, "seu nome completo é: " + nome + " " + sobrenome,"nome completo", JOptionPane.INFORMATION_MESSAGE);
+
+
+        Integer dia = Integer.parseInt(JOptionPane.showInputDialog(null, "informe o dia da semana", "Dia", JOptionPane.QUESTION_MESSAGE));
+
+        switch (dia) {
+
+            case 1: JOptionPane.showMessageDialog(null, "Domingo");
+            break;
+
+            case 2: JOptionPane.showMessageDialog(null, "Segunda-feira");
+            break;
+
+            case 3: JOptionPane.showMessageDialog(null, "Terça-feira");
+            break;
+
+            case 4: JOptionPane.showMessageDialog(null, "Quarta-feira");
+            break;
+
+            case 5: JOptionPane.showMessageDialog(null, "Quinta-feira");
+            break;
+
+            case 6: JOptionPane.showMessageDialog(null, "Sexta-feira");
+            break;
+
+            case 7: JOptionPane.showMessageDialog(null, "Sábado");
+            break;
+
+            default: JOptionPane.showMessageDialog(null, "valor de entrada inválido");
+            break;
+        }
     }
+
+
 }
